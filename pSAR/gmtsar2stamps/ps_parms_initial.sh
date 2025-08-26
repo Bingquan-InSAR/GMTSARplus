256a4ef630b49b2f21759e1f0ac97a50fcd6b8ea
Bingquan Li <38463989+Bingquan-InSAR@users.noreply.github.com>
2025-08-26 14:39:47 +0200

    Delete pSAR directory

--- pSAR/gmtsar2stamps/ps_parms_initial.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Set up the environment for MATLAB Runtime
-export LD_LIBRARY_PATH=/usr/local/MATLAB/MATLAB_Runtime/R2022b/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/R2022b/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/R2022b/sys/os/glnxa64
-
-# Run the desired command or script
-ps_parms_initial
-
-
