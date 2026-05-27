3cb14539860fe596f6adbf65b2676bb7ea104994
Bingquan Li <38463989+Bingquan-InSAR@users.noreply.github.com>
2026-05-27 15:06:58 +0800

    Update demo_run.sh

--- demo/demo_run.sh
+++ demo/demo_run.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+set -e
 
+run_sbas.sh \
+  --kml demo/example_aoi.kml \
+  --st 20240101 \
+  --ed 20240131 \
+  --rel_orbit 11 \
+  --tmpbase 12 \
+  --rlook 20 \
+  --azlook 4
