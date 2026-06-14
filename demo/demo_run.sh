2e9c9bb9ea1abbb187aecc3cdc25e1b4003b7c39
Bingquan Li <38463989+Bingquan-InSAR@users.noreply.github.com>
2026-06-14 09:26:26 +0800

    Fix parameters in demo_run.sh script

--- demo/demo_run.sh
+++ demo/demo_run.sh
@@ -2,10 +2,10 @@
 set -e
 
 run_sbas.sh \
-  --kml demo/example_aoi.kml \
+  --kml example_aoi.kml \
   --st 20220101 \
   --ed 20220501 \
-  --rel_orbit  \
-  --tmpbase 32 \
+  --rel_orbit  32\
+  --tmpbase 24 \
   --rlook 20 \
   --azlook 4
